dc7b4aca05602f1380fe1b346bbc029018289a05
JRKnott <johnknott09@gmail.com>
2025-04-17 11:32:35 -0400

    Update Main.py with Comments (Final)
    Clears up some of the comments. Hopefully this works.

--- NERStreamlitApp/Main.py
+++ NERStreamlitApp/Main.py
@@ -1,9 +1,9 @@
-# imports the necessary libraries
+# Imports the necessary libraries
 import streamlit as st
 import spacy
 from spacy import displacy
 
-#set up spacy pipeline
+# Sets up spacy pipeline
 nlp = spacy.load('en_core_web_sm')
 # title and header
 st.title('My NER Application')
@@ -13,15 +13,15 @@ st.subheader("Please Input your Text")
 # These below help ensure that user inputs stay when the website is reloaded
 # st.session_state helps keep data in place even when buttons are clicked and things are reloaded
 
-#this saves label colors
+# This saves label colors
 if 'label_colors' not in st.session_state:
     st.session_state.label_colors = {}
 
-# this saves custom patterns
+# This saves custom patterns
 if 'custom_patterns' not in st.session_state:
     st.session_state.custom_patterns = []
 
-#This creates an example text, which is the first line from a book I am reading
+# This creates an example text, which is the first line from a book I am reading, Metamorphosis
 if 'user_text' not in st.session_state:
     st.session_state.user_text = (
         'One morning, when Gregor Samsa woke from troubled dreams, '
@@ -34,45 +34,50 @@ if user_file is not None:
     uploaded_text = user_file.read().decode("utf-8")
     st.session_state.user_text = uploaded_text          
     st.success("File Uploaded")
-# creates an area for text that can be edited
+    
+# Creates an area for text that can be edited
 st.text_area(
     "Enter text:",
     key="user_text",   
     height=200
 )
-# sets up the spacy Entity Ruler and Spacy Pipeline
+# Sets up the spacy Entity Ruler and Spacy Pipeline
 if 'entity_ruler' in nlp.pipe_names:
     nlp.remove_pipe('entity_ruler')
 ruler = nlp.add_pipe('entity_ruler', before='ner')
 ruler.add_patterns(st.session_state.custom_patterns)
 
-#This reapplies the stored custom patterns from earlier.
+# This reapplies the stored custom patterns from earlier.
 for pat in st.session_state.custom_patterns:
     if pat['label'] not in st.session_state.label_colors:
         st.session_state.label_colors[pat['label']] = "#f0e5dc"
 
-# # processes text with nlp
+# Processes text with nlp
 doc = nlp(st.session_state.user_text)
-# essentially lists raw entities by loop
+
+# Lists raw entities by loop
 if doc.ents:
-    for ent in doc.ents: # loops through each entity
-        st.write(f"**{ent.text}** - {ent.label_}") # dislplays text and its corresponding label
+    # Loops through each entity
+    for ent in doc.ents: 
+        # Displays text and its corresponding label
+        st.write(f"**{ent.text}** - {ent.label_}") 
 else:
     st.write("0 entities found.")
-
+    
+# Label for printed visualization
 st.header("Processed Text")
 
-#Should get custom colors, not sure if working
+# Should get custom colors, not sure if it is working entirely properly
 options = {"colors": st.session_state.label_colors}
 html = displacy.render(doc, style="ent", options=options)
 st.markdown(html, unsafe_allow_html=True)
 
-#creates custom labels and patterns
+# Creates custom labels and patterns
 st.subheader('Define Custom Entity Patterns')
 custom_label = st.text_input('Entity Label', placeholder='e.g., Person')
 custom_pattern = st.text_input('Entity pattern (text)', placeholder='e.g., George Washington')
 
-#this section is an if else with in an if statement that shows errors if one is entered without the other, i.e labels and patterns
+# This section is an if else with in an if statement that shows errors if one is entered without the other, i.e labels and patterns.
 if st.button('Add pattern'):
     if not custom_label.strip() or not custom_pattern.strip():
         st.error('Label and pattern are both necessary')
@@ -84,15 +89,18 @@ if st.button('Add pattern'):
         })
         st.success(f"Added pattern: [{custom_label} → {custom_pattern}]")
 
-# this shows the current custom patterns in the session state
+# This part shows the current custom patterns in the session state:
+
+# Header
 st.header('Custom Patterns:')
-#organizes patterns visualize
+
+# Organizes patterns visualize
 if st.session_state.custom_patterns:
     for i, pat in enumerate(st.session_state.custom_patterns, start=1):
         st.write(f"{i}. Label: {pat['label']}, Pattern: {pat['pattern']}")
 else:
     st.write('No custom patterns added yet.')
-#clears buttons, clears the nlp pipeline
+# Clears buttons, clears the nlp pipeline
 if st.button('Clear Patterns'):  
     st.session_state.custom_patterns = []
     if 'entity_ruler' in nlp.pipe_names:
